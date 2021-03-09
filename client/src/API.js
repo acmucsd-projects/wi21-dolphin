8da6ab279fbdeb55e51be1e904e69510fa590031
Sumadhwa13 <>
2021-03-09 00:46:56 -0600

    Added API to streamline backend requests from frontend

---
+++ client/src/API.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const serverUrl = "localhost:5000";
+
+const API = {
+    getAllHobbies: function() {
+        return axios.get(`${serverUrl}/hobbies/all`);
+    },
+    getOneHobby: function(hobby) {
+        return axios.get(`${serverUrl}/hobbies/one?name=` + hobby);
+    },
+    getCategory: function(category) {
+        return axios.get(`${serverUrl}/categories?name=` + category);
+    },
+    getUser: function(username, password) {
+        return axios.get(`${serverUrl}/users?user_name=` + username + `&password=` + password);
+    },
+    postUser: function(username, password) {
+        return axios.post(`${serverUrl}/users?user_name=` + username + `&password=` + password);
+    },
+    getPostsHobby: function(hobby) {
+        return axios.get(`${serverUrl}/posts?hobby=` + hobby);
+    },
+    getPostsUser: function(username) {
+        return axios.get(`${serverUrl}/posts?user_name` + username);
+    },
+    postPost: function(username, content, hobby) {
+        return axios.post(`${serverUrl}/posts?user_name` + username + `&content=` + content + `&hobby=` + hobby);
+    },
+    deletePost: function(hobby) {
+        return axios.delete(`${serverUrl}/posts?hobby=` + hobby);
+    }
+
+}
+
+export default API;
