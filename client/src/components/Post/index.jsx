0fe7087b340a0b695aa78a0270f4b894825c2fb4
Dududungha <jacoblin1127@gmail.com>
2021-03-09 10:34:09 -0800

    Start of the subpage

---
+++ client/src/components/Post/index.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+// This should be an object stored in the database that has:
+// title, description, upvote? downvote?, id?
+function Post(props) {
+    return (
+        <div>
+            Post
+        </div>
+    )
+}
+
+export default Post;
