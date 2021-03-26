1a0a508bbb856aa1fdc85be569d2d31feff41c23
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2021-03-25 18:20:03 -0700

    Profile backend development
    Work in Progress

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,9 +1,24 @@
+import React, { useState } from 'react';
+import EdiText from 'react-editext'
+
 function Profile(){
+    var biographyText = "Enter Biography Here"
+    
+    const saveBiography = val => {
+        biographyText = val;
+      }
+    
     return (
-        <div>
-            This is the profile
-        </div>
+        <EdiText
+        type='text'
+        value={biographyText}
+        onSave={saveBiography}
+      />
     )
+
+    
+    
 }
+
 
 export default Profile
