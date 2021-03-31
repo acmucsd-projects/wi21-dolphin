79d3717fc20b8e9a1ffb03badc570fccd976657b
Sumadhwa13 <>
2021-03-31 01:35:39 -0500

    More heroku stuff

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use('/hobbies', hobbyRouter);
 app.use('/posts', postRouter);
 
 dotenv.config();
-console.log(process.env.DB_URL);
+console.log(process.env);
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
