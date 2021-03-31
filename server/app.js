a777241906e9a93ab197443c0643578a23fb9127
Sumadhwa13 <>
2021-03-31 02:30:22 -0500

    pray3

--- server/app.js
+++ server/app.js
@@ -8,14 +8,10 @@ const hobbyRouter = require('./routes/hobbyRoute');
 const postRouter = require('./routes/postRoute');
 const app = express();
 
-app.use(logger('dev'));
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  next();
-});
+
+app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
